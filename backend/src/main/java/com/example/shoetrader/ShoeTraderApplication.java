36bffab8bf271674c24009fbf2760adfcbff8a3e
mbarcenas <misael.barcenas@yahoo.com>
2024-05-02 17:11:02 -0500

    first commit

---
+++ backend/src/main/java/com/example/shoetrader/ShoeTraderApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoetrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeTraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeTraderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/shoetrader/ShoeTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoetrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeTraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
